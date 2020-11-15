71268339b760c65861ef802b221b24a1f049b3ac
Daichi <s1911404@s.tsukuba.ac.jp>
2020-11-15 22:45:36 +0900

    平面上にオブジェクト…？

---
+++ Assets/Spawn.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class Spawn : MonoBehaviour
+{
+    [SerializeField]
+    GameObject objectPrefab;
+    public TrackableType type;
+
+    ARRaycastManager raycastManager;
+    List<ARRaycastHit> hitResults = new List<ARRaycastHit>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        raycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(raycastManager.Raycast(Input.GetTouch(0).position,hitResults, TrackableType.All))
+            {
+                Instantiate(objectPrefab, hitResults[0].pose.position, Quaternion.identity);
+            }
+        }
+    }
+}
